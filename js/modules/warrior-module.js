982164f056344cdf7bfffa35c44688c58b1c0449
sigurdstuge <sigurd.stuge@gmail.com>
2026-05-29 00:25:12 +0200

    Adding Warrior list in module and gathering-army.js file

---
+++ js/modules/warrior-module.js
@@ -0,0 +1,50 @@
+const warriorsObjects = [
+  {
+    name: "Snake",
+    image: "warrior-1.jpg",
+    price: "15",
+    buy: "add to basket",
+  },
+  {
+    name: "Giant",
+    image: "warrior-2.jpg",
+    price: "15",
+    buy: "add to basket",
+  },
+  {
+    name: "Big Axe",
+    image: "warrior-3.jpg",
+    price: "20",
+    buy: "add to basket",
+  },
+  {
+    name: "Thief",
+    image: "warrior-4.jpg",
+    price: "20",
+    buy: "add to basket",
+  },
+  {
+    name: "Tanks",
+    image: "warrior-5.jpg",
+    price: "25",
+    buy: "add to basket",
+  },
+  {
+    name: "Bersker",
+    image: "warrior-6.jpg",
+    price: "30",
+    buy: "add to basket",
+  },
+  {
+    name: "elephant",
+    image: "elephant.png",
+    price: "5",
+    buy: "add to basket",
+  },
+  {
+    name: "catapult",
+    image: "catapult.png",
+    price: "5",
+    buy: "add to basket",
+  },
+];
