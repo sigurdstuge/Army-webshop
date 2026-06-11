e2f7ad774dad1db9bcbb112518965f95e5828c32
sigurdstuge <sigurd.stuge@gmail.com>
2026-06-11 16:03:20 +0200

    Implementing functionality for local storage.

--- js/gathering-army.js
+++ js/gathering-army.js
@@ -1,8 +1,26 @@
 import warriorsObjects from "./modules/warrior-module.js";
+import resourceStorage from "./modules/resource-storage.js";
+
+const storage = resourceStorage;
+
+const goldCount = document.querySelector("#gold-count");
+const metalCount  = document.querySelector("#metal-count");
+const woodCount = document.querySelector("#wood-count");
+
+
 //henter html elementer
 const searchFilter = document.querySelector("#search-filter");
 const searchResult = document.querySelector("#search-result");
 const warriorContainer = document.querySelector("#warrior-container");
+
+
+let goldAmmount = parseInt(storage.getItem("gold")) || 0;
+let metalAmmount = parseInt(storage.getItem("metal")) || 0;;
+let woodAmmount = parseInt(storage.getItem("wood")) || 0;
+
+goldCount.innerHTML = goldAmmount;
+metalCount.innerHTML = metalAmmount;
+woodCount.innerHTML = woodAmmount;
 
 const warriorData = (filterData) => {
 	filterData.forEach((item, index) => {
